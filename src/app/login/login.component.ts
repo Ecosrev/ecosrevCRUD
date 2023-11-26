779fda26aac3767655a19359c215f226e24f7e9f
marianakakimoto <marianakakimoto@gmail.com>
2023-11-26 18:20:08 -0300

    Finalização do front

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  
+  email: string = '';
+  password: string = '';
+
+  constructor(private router: Router) {}
+
+  validateLoginForm(): boolean {
+    // Verificando se os campos não estão vazios
+    if (this.email.trim() === '' || this.password.trim() === '') {
+      alert('Por favor, preencha todos os campos!');
+      return false;
+    }
+
+    // Expressão regular para validar o email
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    // Verifica se o email corresponde ao padrão de email válido
+    if (!emailRegex.test(this.email)) {
+      alert('Por favor, insira um e-mail válido.');
+      return false;
+    }
+
+    // Verifica se a senha tem exatamente 6 caracteres
+    if (this.password.length !== 6) {
+      alert('A senha deve ter exatamente 6 caracteres.');
+      return false;
+    }
+
+    return true; // Retorna true se a validação for bem-sucedida
+  }
+
+  onSubmit(): void {
+    if (this.validateLoginForm()) {
+      // Se a validação for bem-sucedida, redirecione para a página desejada 
+      setTimeout(() => {
+        this.router.navigate(['/central']); // Substitua '/central' pela rota desejada
+      }, 200); // Tempo em milissegundos
+    }
+  }
 }
